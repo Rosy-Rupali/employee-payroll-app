7df21a35699d157dfafec446646eece14ba40da5
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-17 23:36:09 +0530

    added: event listener on salary range to display appropriate value

---
+++ js/new_payroll_form.js
@@ -0,0 +1,7 @@
+// UC 8
+const salary = document.querySelector('#salary');
+const salaryOutput = document.querySelector('.salary-output');
+salaryOutput.textContent = salary.value;
+salary.addEventListener('input', function(){
+    salaryOutput.textContent = salary.value;
+});
